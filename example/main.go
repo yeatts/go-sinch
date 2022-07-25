6ca522a12425cf421ba24735fe99ee63eb9e31f3
theZMC <zcallahan@firstorion.com>
2022-07-25 14:05:19 -0500

    feat: added minimal example

---
+++ example/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"go-sinch/pkg/sms"
+)
+
+func main() {
+	client := sms.NewClient().US().WithAuthToken("authToken").WithPlanID("planID")
+
+	req := client.NewSendRequest().
+		To("+12345678901").
+		From("+12345678901").
+		WithBody("Hello World!")
+
+	if _, err := req.Execute(); err != nil {
+		fmt.Println(err)
+	}
+}
